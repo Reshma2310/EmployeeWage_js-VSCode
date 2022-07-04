522d5fc2e603ea42cf286124c069c350e8aad3c5
Reshma2310 <dreshma253@gmail.com>
2022-07-04 18:36:26 +0530

    Calculating Total wage of employee for Month(20 Days)

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const No_Of_Working_Days = 20;
 //UC3: Using Functions
 function getWorkingHours(empCheck){
     //UC2: Switch Case Usage
@@ -15,7 +16,11 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Working Hours: " + empHrs + ", Employee Wage: " + empWage);
+//UC4: Wage of Employee for 20 Days
+let totalEmpHrs = 0;
+for (let day =0; day < No_Of_Working_Days; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Working Hours: " + totalEmpHrs + ", Employee Wage: " + empWage);
