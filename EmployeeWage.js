ea95e63686c09c5dde56c452e8ef26885723a535
Reshma2310 <dreshma253@gmail.com>
2022-07-05 16:58:32 +0530

    Storing Daily wage of Employee in Array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,11 @@
-const IS_PART_TIME = 1;
+ const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const No_Of_Working_Days = 20;
 const MAX_HRS_IN_MONTH = 160;
+
 //UC3: Using Functions
 function getWorkingHours(empCheck){
     //UC2: Switch Case Usage
@@ -17,13 +18,22 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
-//UC5: Wage of Employee for 20 Days
+function calDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+//UC6: Store daily wage of each day
 let totalEmpHrs = 0;
+let empHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
 while (totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < No_Of_Working_Days) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    empHrs = getWorkingHours(empCheck);
+    totalEmpHrs = totalEmpHrs + empHrs;
+    empDailyWageArr.push(calDailyWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+let empWage = calDailyWage(totalEmpHrs);
+console.log(empDailyWageArr);
 console.log("Total Days: " + totalWorkingDays  + ", Total Working Hours: " + totalEmpHrs + ", Employee Wage: " + empWage);
