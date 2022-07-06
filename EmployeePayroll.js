b2f4b3d1c8740a1284fbb758aecb9222a32a4198
Reshma2310 <dreshma253@gmail.com>
2022-07-06 20:20:38 +0530

    Usage of Constructor, get & set along with Method

---
+++ EmployeePayroll.js
@@ -0,0 +1,28 @@
+class EmployeePayrollData
+{
+    id; 
+    salary;
+    gender;    
+    // Constructor
+    constructor(id, name, salary)
+    {
+        this.id = id;
+        this.name = name
+        this.salary = salary;
+    }
+    //Getter & Setter Method
+    get name() { return this._name; }
+    set name(name)
+    {
+        this._name = name;
+    }  
+    //Method
+    toString() {
+        
+        return "Id: " + this.id + ", Name: " + this.name + ", Salary: " + this.salary;
+    }
+}
+let employeePayrollData = new EmployeePayrollData(1, "Reshma", 40000);
+console.log(employeePayrollData.toString());
+employeePayrollData.name = "Vahidha";
+console.log(employeePayrollData.toString());
