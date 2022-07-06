0a6376b5a47b714eeb90cbd800070ebaed8648ed
Reshma2310 <dreshma253@gmail.com>
2022-07-06 21:28:08 +0530

    Extend Employee payroll data to store gender & startDate

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -2,27 +2,35 @@ class EmployeePayrollData
 {
     id; 
     salary;
-    gender;    
-    // Constructor
-    constructor(id, name, salary)
+    gender; 
+    startDate;   
+    //Constructor
+    constructor(...params)
     {
-        this.id = id;
-        this.name = name
-        this.salary = salary;
+        this.id = params[0];
+        this.name = params[1]
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
     //Getter & Setter Method
     get name() { return this._name; }
-    set name(name)
-    {
-        this._name = name;
-    }  
+    set name(name) { this._name = name; }  
     //Method
     toString() {
-        
-        return "Id: " + this.id + ", Name: " + this.name + ", Salary: " + this.salary;
+        const options = { year: 'numeric', month: 'numeric', day: 'numeric'};
+        const empDate = this.startDate == undefined ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US", options);
+        return "Id: " + this.id + ", Name: " + this.name + ", Salary: " + this.salary + ", Gender: " + this.gender + ", StartDate: " + this.startDate;
     }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Reshma", 40000);
 console.log(employeePayrollData.toString());
+employeePayrollData.id = 2;
 employeePayrollData.name = "Vahidha";
+employeePayrollData.salary = "60000";
 console.log(employeePayrollData.toString());
+//const date = new Date();
+//let newEmployeeData = new EmployeePayrollData(3, "Reshu", 50000, "Female", date.toLocaleDateString());
+let newEmployeeData = new EmployeePayrollData(3, "Reshu", 50000, "Female", new Date());
+console.log(newEmployeeData.toString());
