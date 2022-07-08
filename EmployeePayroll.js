99e886c86c2486d7b3d0acda8d27b83d2018af50
Reshma2310 <dreshma253@gmail.com>
2022-07-08 15:39:10 +0530

    Regex for Salary, Gender, Mail & Zipcode

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,10 +1,5 @@
-class EmployeePayrollData
-{
-    id; 
-    salary;
-    gender; 
-    startDate;   
-    //Constructor
+class EmployeePayrollData {
+    
     constructor(...params)
     {
         this.id = params[0];
@@ -13,39 +8,144 @@ class EmployeePayrollData
         this.gender = params[3];
         this.startDate = params[4];
     }
-    //Getter & Setter Method
-    get name() { return this._name; }
-    set name(name) 
-    { 
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
-        if (nameRegex.test(name)){
+
+    //getter and setter method
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        let nameRegex = RegExp('^[A-z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name))
             this._name = name;
-        }            
-        else throw 'Name should start with Capital letter and contain min 4 letters';
-     }  
-    //Method
-    toString() {
-        const options = { year: 'numeric', month: 'numeric', day: 'numeric'};
-        const empDate = this.startDate == undefined ? "undefined" :
-                        this.startDate.toLocaleDateString("en-US", options);
-        return "Id: " + this.id + ", Name: " + this.name + ", Salary: " + this.salary + ", Gender: " + this.gender + ", StartDate: " + this.startDate;
+        else throw 'Name is incorrect';
+    }
+    get id() 
+    {
+        return this._id;
+    }
+    set id(id)
+    {
+        let idCheck = RegExp('^[1-9]{1}([0-9]*)$')
+        if(idCheck.test(id))
+        {
+            this._id = id;
+        }
+        else
+        {
+            throw "ID should be non zero positive number";
+        }
+    }
+    get salary() 
+    {
+        return this._salary;
+    }
+    set salary(salary)
+    {
+        let salaryCheck = RegExp('^[1-9]{1}([0-9]*)([.]*)[0-9]*$')
+        if(salaryCheck.test(salary))
+        {
+            this._salary = salary;
+        }
+        else
+        {
+            throw "Salary should be non zero positive number";
+        }
+    }
+    get gender() 
+    {
+        return this._gender;
+    }
+    set gender(gender)
+    {
+        let genderCheck = RegExp('^[M|F]{1}$')
+        if(genderCheck.test(gender))
+        {
+            this._gender = gender;
+        }
+        else
+        {
+            throw "Gender should be either F or M";
+        }
+    }
+    get startDate() 
+    {
+        return this._startDate;
+    }
+    set startDate(startDate)
+    {
+        let today = new Date();
+        let dateCheck = RegExp("^[0-9]{1,2}[/][0-9]{1,2}[/][0-9]{4}$");
+        if(startDate <= today.toLocaleDateString())
+        {
+            if(dateCheck.test(startDate))
+            {
+                this._startDate = startDate;
+            }
+            else
+            {
+                throw "Format Mismatch";
+            }
+        }
+        else
+        {
+            throw "Start Date should be less than or equal to current Date";
+        }
+    }
+    get email() 
+    {
+        return this._email;
+    }
+    set email(email)
+    {
+        let emailCheck = RegExp('^[0-9a-z]{1,}([.+-]?)[0-9a-z]{1,}(@)[a-z0-9]{1,}(.)(com|co|net)((.in|.com|.au)*)$')
+        if(emailCheck.test(email))
+        {
+            this._email = email;
+        }
+        else
+        {
+            throw "Invalid Email ID";
+        }
+    }
+    get zipCode() 
+    {
+        return this._zipCode;
+    }
+    set zipCode(zipCode)
+    {
+        let zipCodeCheck = RegExp('^[0-9]{6}$')
+        if(zipCodeCheck.test(zipCode))
+        {
+            this._zipCode = zipCode;
+        }
+        else
+        {
+            throw "Invalid zipCode";
+        }
+    }
+    
+    Employee()
+    {
+        return "id =" + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + this.startDate +
+        ",Email: " + this.email + ",Zip Code: " + this.zipCode;
     }
 }
 {
-    let employeePayrollData = new EmployeePayrollData(1, "Reshma", 40000);
-    console.log(employeePayrollData.toString());
     try
     {
-    employeePayrollData.id = 2;
-    employeePayrollData.name = "Vahidha";
-    employeePayrollData.salary = "60000"
-    console.log(employeePayrollData.toString());
+    let employeePayrollData = new EmployeePayrollData(1, "Reshma", 40000, "F", new Date("8/7/2022").toLocaleDateString(), "reshma@gmail.com", 333329);
+    console.log(employeePayrollData.Employee());
+    employeePayrollData = new EmployeePayrollData(2, "Basith", 40000, "M", new Date("6/7/2022").toLocaleDateString(), "basith@gmail.com", 555048);
+    console.log(employeePayrollData.Employee());
+    employeePayrollData.id = 3;
+    employeePayrollData.name = "Tanvir";
+    employeePayrollData.salary = 20000;
+    employeePayrollData.gender = "F";
+    employeePayrollData.startDate = new Date("9-7-2022").toLocaleDateString();    
+    console.log(employeePayrollData.Employee());
     }
-    catch(excep){
-        console.error(excep);
+    catch(ex)
+    {
+        console.error(ex);
     }
-    //const date = new Date();
-    //let newEmployeeData = new EmployeePayrollData(3, "Reshu", 50000, "Female", date.toLocaleDateString());
-    let newEmployeeData = new EmployeePayrollData(3, "Reshu", 50000, "Female", new Date());
-    console.log(newEmployeeData.toString());
 }
